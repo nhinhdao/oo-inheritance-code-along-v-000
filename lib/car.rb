9ed6c7048776a35424e7ca7dc33c693e7ee2248e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-01 01:09:13 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,18 +1,16 @@
 require_relative "./vehicle.rb"
 
-class Car
-    attr_accessor :wheel_size, :wheel_number
+class Car < Vehicle
+    #all these attributes and behaviors are adopted from parents class Vehicle
+    # attr_accessor :wheel_size, :wheel_number
+    #
+    # def initialize(wheel_size, wheel_number)
+    #     @wheel_size = wheel_size
+    #     @number = wheel_number
+    # end
 
-    def initialize(wheel_size, wheel_number)
-        @wheel_size = wheel_size
-        @number = wheel_number
-    end
-
+    #this method is defferent so it needs to defines separatedly
     def go
         "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-    end
-
-    def fill_up_tank
-        "filling up!"
     end
 end
