ad841f06a662f4b8fb3021d7df502be76da37a26
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-01 01:10:39 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,16 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+    #all these attributes and behaviors are adopted from parents class Vehicle
+    # attr_accessor :wheel_size, :wheel_number
+    #
+    # def initialize(wheel_size, wheel_number)
+    #     @wheel_size = wheel_size
+    #     @number = wheel_number
+    # end
+
+    #this method is defferent so it needs to defines separatedly
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
